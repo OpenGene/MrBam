16a18a8b95a69813f09da63681744cd88a800ca9
ylxdzsw <ylxdzsw@gmail.com>
2016-09-02 10:42:40 +0800

    aggregate reads by pos and base

--- src/main.jl
+++ src/main.jl
@@ -4,6 +4,7 @@ using ArgParse
 using OhMyJulia
 
 include(rel"bam.jl")
+include(rel"aggregate.jl")
 
 const o = let
     s = ArgParseSettings()
@@ -16,10 +17,14 @@ const o = let
             help = "bam file contains origin reads info. There must be a corresponding .bai file in the same directory"
             required = true
         "--ref", "-r"
-            help = "reference file. If not provided, consider mode of reads as the ref"
+            help = "reference file. If not provided, consider the mode of reads as the ref"
         "--verbos", '-v'
             help = "output debug info"
             nargs = 0
+        "--quality", "-q"
+            help = "drop bases whose qulity is less than this"
+            arg_type = Int
+            default = 20
     end
 
     parse_args(s) |> to_module
